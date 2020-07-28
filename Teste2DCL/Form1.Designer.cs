03f2e845f828ecd28e7db0d8a0f184e7dd69c63c
danielfonsecalaves <alvesdaniel@hotmail.com>
2020-07-28 11:40:53 -0300

    Testes DCL

---
+++ Teste2DCL/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Teste2DCL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValorMinuto = new System.Windows.Forms.TextBox();
+            this.txtDuracaoSegundos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotalValor = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor por minuto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Duração:";
+            // 
+            // txtValorMinuto
+            // 
+            this.txtValorMinuto.Location = new System.Drawing.Point(167, 67);
+            this.txtValorMinuto.Name = "txtValorMinuto";
+            this.txtValorMinuto.Size = new System.Drawing.Size(100, 22);
+            this.txtValorMinuto.TabIndex = 2;
+            // 
+            // txtDuracaoSegundos
+            // 
+            this.txtDuracaoSegundos.Location = new System.Drawing.Point(167, 103);
+            this.txtDuracaoSegundos.Name = "txtDuracaoSegundos";
+            this.txtDuracaoSegundos.Size = new System.Drawing.Size(100, 22);
+            this.txtDuracaoSegundos.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "(em segundos)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Total:";
+            // 
+            // lblTotalValor
+            // 
+            this.lblTotalValor.AutoSize = true;
+            this.lblTotalValor.Location = new System.Drawing.Point(164, 157);
+            this.lblTotalValor.Name = "lblTotalValor";
+            this.lblTotalValor.Size = new System.Drawing.Size(21, 17);
+            this.lblTotalValor.TabIndex = 6;
+            this.lblTotalValor.Text = " - ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 314);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTotalValor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDuracaoSegundos);
+            this.Controls.Add(this.txtValorMinuto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Valor Chamada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValorMinuto;
+        private System.Windows.Forms.TextBox txtDuracaoSegundos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotalValor;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Teste2DCL/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste2DCL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal Total = 0;
+            var valorFracao = decimal.Parse(txtValorMinuto.Text) / 10;
+
+            if (int.Parse(txtDuracaoSegundos.Text) <= 60)
+            {
+                Total = decimal.Parse(txtValorMinuto.Text);
+            }
+            else
+            {
+                for (int i = 60; i <= int.Parse(txtDuracaoSegundos.Text); i++)
+                {
+                    if (i % 6 == 0)
+                    {
+                        Total += valorFracao;
+                    }
+                    
+                }
+                Total += decimal.Parse(txtValorMinuto.Text);
+            }
+
+            lblTotalValor.Text = Total.ToString();
+        }
+    }
+}
